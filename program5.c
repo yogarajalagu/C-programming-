528419e3762fb8d5a2c6bc7d6034cabc561f8e51
yogarajalagu <26bst059@hicas.ac.in>
2026-09-08 22:59:00 +0530

    Implement number swapping in program5.c
    Fix the swapping logic and print statement in the program.

---
+++ program5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: ");
+    a = %d, a, b;
+    
+    return 0;
+}
