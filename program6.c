0f2fff76d19e1311766f496602a7466f14f4bdf8
yogarajalagu <26bst059@hicas.ac.in>
2026-09-08 22:59:48 +0530

    Add prime number checking program in program6.c

---
+++ program6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (n > 1 && flag == 0)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
